af1f7ec99d116c32a2239cfa5ff159ee8fa4df10
Tiago França <github@tiagofranca.com>
2025-06-03 10:08:35 -0300

    added dependencies
    Changes to be committed:
    	new file:   .editorconfig
    	new file:   .gitignore
    	new file:   .prettierignore
    	new file:   .prettierrc
    	modified:   .vite/deps/_metadata.json
    	modified:   .vite/deps/package.json
    	modified:   .vscode/settings.json
    	modified:   index-v2.html
    	modified:   index.html
    	new file:   package-lock.json
    	new file:   package.json
    	modified:   script.js

--- script.js
+++ script.js
@@ -3,20 +3,12 @@ const ingredientesContainer = document.querySelector('[data-name="ingredientes"]
 const modoDePreparoContainer = document.querySelector('[data-name="modo-de-preparo"]');
 
 const titulo = 'Receita de Bolo de Fubá';
-const ingredientes = [
-    '1 ovo',
-    '1 xícara de leite',
-    '1 colher de sopa de manteiga',
-];
+const ingredientes = ['1 ovo', '1 xícara de leite', '1 colher de sopa de manteiga'];
 
-const itensPreparo = [
-    'Bata o ovo e o leite',
-    'Coloque em uma forma untada',
-    'Leve ao forno por 40 minutos',
-]
+const itensPreparo = ['Bata o ovo e o leite', 'Coloque em uma forma untada', 'Leve ao forno por 40 minutos'];
 
 tituloContainer.innerText = titulo;
-ingredientes.forEach(ingrediente => {
+ingredientes.forEach((ingrediente) => {
     const element = document.createElement('li');
 
     // element.innerHTML = `👍🏻 ${ingrediente}`;
@@ -25,7 +17,7 @@ ingredientes.forEach(ingrediente => {
     ingredientesContainer.appendChild(element);
 });
 
-itensPreparo.forEach(item => {
+itensPreparo.forEach((item) => {
     const element = document.createElement('li');
     element.innerText = `👍🏻 ${item}`;
 
